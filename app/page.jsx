881971c5d2179243294da280d87a7af2d0c9c4d3
jfiora <sillonpeludo@gmail.com>
2024-04-01 20:59:26 -0300

    middleware, redirect and stuff

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
 import { LoginForm } from '@/components/LoginForm';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+    const session = await getServerSession(authOptions);
+
+    if (session) {
+        redirect('dashboard');
+    }
     return (
         <main>
             <LoginForm />
